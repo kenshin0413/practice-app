0d57d6b7cd1b63fb04d050054ac38fb59e75959f
kenshin0413 <kenshin1234569@gmail.com>
2025-09-06 00:50:57 +0900

    ボタン押したら数字追加とリスト削除

--- practice-app/practice-app/ContentView.swift
+++ practice-app/practice-app/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+struct Number: Identifiable {
+    let id = UUID()
+    let value: Int
+}
+
 struct ContentView: View {
+    @State var items = [Number(value: 1), Number(value: 2), Number(value: 3)]
+    @State var isEditing = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(items) { item in
+                        Text("数字は\(item.value)")
+                    }
+                    .onDelete { indexSet in
+                        items.remove(atOffsets: indexSet)
+                    }
+                }
+                Button("数字を追加") {
+                    let newvalue = Int.random(in: 0...100)
+                    items.append(Number(value: newvalue))
+                }
+            }
+            .navigationTitle("数字リスト")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(isEditing ? "完了" : "編集") {
+                        withAnimation {
+                            isEditing.toggle()
+                        }
+                    }
+                }
+            }
+            .environment(\.editMode, .constant(isEditing ? EditMode.active : EditMode.inactive))
         }
-        .padding()
     }
 }
 
