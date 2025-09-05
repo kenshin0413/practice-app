d555851eaea5fed827df22c60d05eb1423f04550
kenshin0413 <kenshin1234569@gmail.com>
2025-09-05 14:27:28 +0900

    初期

---
+++ practice-app/practice-app/practice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_appApp.swift
+//  practice-app
+//
+//  Created by miyamotokenshin on R 7/09/05.
+//
+
+import SwiftUI
+
+@main
+struct practice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
